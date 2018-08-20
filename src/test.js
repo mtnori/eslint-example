af9270a8d4dce07f49e8f299c68e8e75e4890ae9
mtnori <moto44.noriyuki@gmail.com>
2018-08-21 07:58:38 +0900

    update package.json

--- src/test.js
+++ src/test.js
@@ -2,6 +2,9 @@
 
 import type { Map as MapType } from 'immutable';
 import { Map } from 'immutable';
+import sum from './test2';
 
 const map: MapType<string, number> = Map({ test: 1234 });
 console.log(map.toJS());
+
+sum(1, 1);
---
+++ src/test2.js
@@ -0,0 +1,4 @@
+// @flow
+
+const sum = (arg1: number, arg2: number) => arg1 + arg2;
+export default sum;
